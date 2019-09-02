70b133e30f56b93ea9ac136d20e0daa4691531e2
Alex Ford <alex96ford19@gmail.com>
2019-09-02 10:37:35 +0100

    initial commit

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+    $('#navigation-bar-js').html('<header class="navigation-container"><nav><ul class="nav-list"><li><a href="html.html" class="nav-button">HTML</a></li><li><a href="css.html" class="nav-button">Css</a></li><li><a href="javascript.html" class="nav-button">JavaScript</a></li><li><a href="FAQ.html" class="nav-button">FAQ</a></li></ul></nav></header>')
+});
